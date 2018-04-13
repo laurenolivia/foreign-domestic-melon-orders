9d65d0dcb3b0e572607d91fcae971c11bca67add
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-13 13:04:56 -0700

    Added get_total() method to InternationalMelonOrder class and updated parent class get_total() method to accommodate christmas melon species

--- melons.py
+++ melons.py
@@ -12,7 +12,11 @@ class AbstractOrder(object):
     def get_total(self):
         """Calculate price, including tax."""
 
-        base_price = 5
+        if self.species = "Christmas melon":
+            base_price = 7.5
+        else:
+            base_price = 5
+
         total = (1 + self.tax) * self.qty * base_price
 
         return total
@@ -37,14 +41,21 @@ class InternationalMelonOrder(AbstractOrder):
     def __init__(self, species, qty, country_code):
         super(InternationalMelonOrder, self).__init__(species, qty)
         self.country_code = country_code
-
-    #if qty < 10:
-        self.total = original total + 3
-    else:
-        return total         
+     
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        total = super(InternationalMelonOrder, self).get_total()
+
+        if self.qty < 10:
+            return total + 3
+        else:    
+            return total
+
+
+
 
